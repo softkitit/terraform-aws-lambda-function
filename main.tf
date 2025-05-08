d426ef366bee6f74916954eb50834cba272719d7
Vitalii Samofal <vitalii.samofal@softkit.dev>
2025-05-08 13:43:48 +0100

    fix: just hardcode ignoring image_uri

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ resource "aws_lambda_function" "this" {
   depends_on = [module.cloudwatch_log_group]
 
   lifecycle {
-    ignore_changes = var.ignore_changes
+    ignore_changes = [ var.image_uri ]
   }
 }
 
